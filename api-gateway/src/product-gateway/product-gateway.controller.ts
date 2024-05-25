dd69bd8da16f92e4ce37c8d53cab07833f8ab67d
EyadTamer999 <eyadhassan2002@gmail.com>
2024-05-25 04:04:47 +0300

    refactor: Refactor product-gateway controller and remove unused parameters

--- api-gateway/src/product-gateway/product-gateway.controller.ts
+++ api-gateway/src/product-gateway/product-gateway.controller.ts
@@ -6,30 +6,40 @@ import { AddToCartDTO } from './DTO/addToCart.dto';
 
 @Controller('Product')
 export class ProductGatewayController {
-    constructor(private readonly productGatewayService: ProductGatewayService) {}
+    constructor(private readonly productGatewayService: ProductGatewayService) { }
+    
+    // DONE
     @Get('getAllProducts')
     async getAllProducts(): Promise<any> {
         return this.productGatewayService.getAllProducts();
     }
+
+    // DONE
     @Get('getTopProducts')
     async getTopProduct(): Promise<any> {
         return this.productGatewayService.getTopProducts();
     }
+
+    // DONE
     @Get('getTopOffers')
     async getTopOffers(): Promise<any> {
         return this.productGatewayService.getTopOffers();
     }
+
+    // DONE
     @Get('getCategory/:category')
     async getCategory(@Param('category') category: string): Promise<any> {
         return this.productGatewayService.getCategory(category);
     }
 
+    // DONE
     @Get('getProduct/:id')
     async getProduct(@Param('id') id: string): Promise<any> {
 
         return this.productGatewayService.getProduct(id);
     }
 
+    // DONE
     @Delete('deleteProduct/:id')
     async deleteProduct(@Param('id') id: any, @Req() request: any): Promise<any> {
         const jwtToken = request.headers.authorization?.replace('Bearer ', '');
@@ -38,29 +48,36 @@ export class ProductGatewayController {
     }
 
 
+    // DONE
     @Post('addToCart')
     async addToCart(@Body() body: AddToCartDTO, @Req() request: any): Promise<any> {
+        console.log('Body',body);
         const jwtToken = request.headers.authorization?.replace('Bearer ', '');
         return this.productGatewayService.addToCart(body, jwtToken);
     }
+
+    // TODO mesh betmsa7 fel database
     @Delete('deleteFromCart/:id')
     async deleteFromCart(@Param('id') id: string,  @Req() request: any): Promise<any> {
         const jwtToken = request.headers.authorization?.replace('Bearer ', '');
         return this.productGatewayService.deleteFromCart(id, jwtToken);
     }
+
+    // DONE
     @Get('getCart')
     async getCartItems(@Req() request: any): Promise<any> {
         const jwtToken = request.headers.authorization?.replace('Bearer ', '');
         return this.productGatewayService.getCartItems(jwtToken);
     }
     
-
+    // DONE
     @Post('customizeProduct')
     async customizeProduct(@Body() body: any): Promise<any> {
         const { productId, size,color,material } = body;
         return this.productGatewayService.customizeProduct(productId, size,color,material);
     }
-    //Start Review Sevtion
+    
+    // DONE
     @Post('addReview')
     async addReview(@Body() body: any, @Req() request: any): Promise<any> {
         const jwtToken = request.headers.authorization?.replace('Bearer ', '');
@@ -72,12 +89,14 @@ export class ProductGatewayController {
     }
 
     //get all user reviews
+    // DONE
     @Get('getUserReviews')
     async getUserReviews(@Req() request: any): Promise<any> {
         const jwtToken = request.headers.authorization?.replace('Bearer ', '');
         return this.productGatewayService.getUserReviews(jwtToken);
     }   
     //update user review on a product
+    // DONE
     @Put('updateUserReview')
     async updateUserReview(@Body() body: any, @Req() request: any): Promise<any> {
         const jwtToken = request.headers.authorization?.replace('Bearer ', '');
@@ -85,18 +104,20 @@ export class ProductGatewayController {
         return this.productGatewayService.updateUserReview( productId, review, jwtToken);
     }
     //delete user review on a product
+    // DONE
     @Delete('deleteUserReview')
     async deleteUserReview(@Body() body: any, @Req() request: any): Promise<any> {
         const jwtToken = request.headers.authorization?.replace('Bearer ', '');
-        const {  productId} = body;
+        console.log('Body',body);
+        const {productId} = body;
         return this.productGatewayService.deleteUserReview( productId, jwtToken);
     }
 
 
 
 
-    //End Review Section
 
+    //DONE
     @Post('createProduct')
     async createProduct(@Body() product: createProductDto, @Req() request: any): Promise<any> {
        const jwtToken = request.headers.authorization?.replace('Bearer ', '');
@@ -111,20 +132,22 @@ export class ProductGatewayController {
 
 
 
-    //new work!!!!!!
     
+    // DONE
     @Get('getUserFavoriteProducts')
     async getUserFavoriteProducts(@Req() request: any): Promise<any> {
         const jwtToken = request.headers.authorization?.replace('Bearer ', '');
         return this.productGatewayService.getUserFavoriteProducts(jwtToken);
     }
 
+    // DONE
     @Get('getUserWishProducts')
     async getUserWishProducts( @Req() request: any): Promise<any> {
         const jwtToken = request.headers.authorization?.replace('Bearer ', '');
         return this.productGatewayService.getUserWishProducts( jwtToken);
     }
 
+    // DONE
     @Delete('removeProductFromMyWish')
     async removeProductFromMyWish(@Body() body: any, @Req() request: any): Promise<any> {
         const jwtToken = request.headers.authorization?.replace('Bearer ', '');
@@ -132,13 +155,16 @@ export class ProductGatewayController {
         return this.productGatewayService.removeProductFromMyWish(productId, jwtToken);
     }
 
+    // DONE
     @Delete('removeProductFromMyFavorite')
     async removeProductFromMyFavorite(@Body() body: any, @Req() request: any): Promise<any> {
         const jwtToken = request.headers.authorization?.replace('Bearer ', '');
+        console.log(body)
         const {  productId } = body;
         return this.productGatewayService.removeProductFromMyFavorite( productId, jwtToken);
     }
 
+    // DONE
     @Post('postUserFavoriteProduct')
     async postUserFavoriteProduct(@Body() body: any, @Req() request: any): Promise<any> {
         const jwtToken = request.headers.authorization?.replace('Bearer ', '');
@@ -146,6 +172,7 @@ export class ProductGatewayController {
         return this.productGatewayService.postUserFavoriteProduct( productId, jwtToken);
     }
 
+    // DONE
     @Post('postUserWishProduct')
     async postUserWishProduct(@Body() body: any, @Req() request: any): Promise<any> {
         const jwtToken = request.headers.authorization?.replace('Bearer ', '');
